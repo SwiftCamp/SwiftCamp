adc3f55e74703f7f2d9366fd299bbe941a33da6b
Diogo Antunes <diogo.antunes@gmail.com>
2016-05-12 17:40:36 +0100

    :zap: Initial commit containing folder structure and travis file

---
+++ SwiftCamp/UI/ViewControllers/Main/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SwiftCamp
+//
+//  Created by Diogo Antunes on 4/12/16.
+//  Copyright © 2016 SwiftCamp. All rights reserved.
+//
+
+import UIKit
+
+/*
+ -  MVVM
+ -  RxSwift
+ -  TDD
+ -  Dependecy Injection
+ -  Travis
+ */
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
