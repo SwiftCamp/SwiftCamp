aafb6630a740313cd5fe83c1765a4c290e6df5df
Diogo Antunes <diogo.antunes@gmail.com>
2016-05-13 14:43:18 +0100

    Added Tab bar containing People and Schedules items

---
+++ SwiftCamp/UI/SCPeopleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SCPeopleViewController.swift
+//  SwiftCamp
+//
+//  Created by Diogo Antunes on 13/05/16.
+//  Copyright © 2016 SwiftCamp. All rights reserved.
+//
+
+import UIKit
+
+class SCPeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ SwiftCamp/UI/SCScheduleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SCScheduleViewController.swift
+//  SwiftCamp
+//
+//  Created by Diogo Antunes on 13/05/16.
+//  Copyright © 2016 SwiftCamp. All rights reserved.
+//
+
+import UIKit
+
+class SCScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
